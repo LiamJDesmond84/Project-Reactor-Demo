7fa2b950d08bdd4aa8c46c06a9219d7479d580d7
Liam Desmond <liamjdesmond84@gmail.com>
2022-06-19 10:38:13 -0500

    adding exceptions

--- src/main/java/com/liam/projectreactor/exceptions/NetworkException.java
+++ src/main/java/com/liam/projectreactor/exceptions/NetworkException.java
@@ -9,15 +9,14 @@ public class NetworkException extends RuntimeException {
 	
 	String message;
 
-    public NetworkException(String message) {
+	public NetworkException(String message) {
         super(message);
         this.message = message;
     }
 
     public NetworkException(Throwable ex) {
         super(ex);
-//        this.message = message;
-        this.message = ex.getMessage();
+        this.message = message;
     }
 
 }
