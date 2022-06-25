97264ea83fa465bbe689aa23e840bfdf0128884f
Liam Desmond <liamjdesmond84@gmail.com>
2022-06-25 12:47:08 -0500

    Adding imperative method

---
+++ src/main/java/com/liam/projectreactor/imperative/ImperativeExample.java
@@ -0,0 +1,34 @@
+package com.liam.projectreactor.imperative;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImperativeExample {
+	
+	public static void main(String[] args) {
+		
+		List<String> namesList = List.of("Alex", "Ben", "Chloe", "Adam");
+		
+		List<String> result = namesGreaterThanSize(namesList, 3);
+		System.out.println(result);
+		
+
+	}
+	
+	private static List<String> namesGreaterThanSize(List<String> namesList, int x) {
+		
+		List<String> newNamesList = new ArrayList<String>();
+		
+		for(int y = 0; y < namesList.size(); x++) {
+			
+			if(namesList.get(y).length() > 3) {
+				newNamesList.add(namesList.get(y));
+			}
+			
+		}
+		
+		return newNamesList;
+		
+	}
+
+}
