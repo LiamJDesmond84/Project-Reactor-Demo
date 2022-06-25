8579aec546cf24da7b0932f615f31ed2fdd9f7db
Liam Desmond <liamjdesmond84@gmail.com>
2022-06-25 16:08:20 -0500

    Adding Flux method

--- src/main/java/com/liam/projectreactor/services/FluxAndMonoGeneratorService.java
+++ src/main/java/com/liam/projectreactor/services/FluxAndMonoGeneratorService.java
@@ -6,15 +6,22 @@ import reactor.core.publisher.Flux;
 
 public class FluxAndMonoGeneratorService {
 	
+
+	
+	public static void main(String[] args) {
+		
+		FluxAndMonoGeneratorService fluxAndMonoGenServ = new FluxAndMonoGeneratorService();
+		
+		fluxAndMonoGenServ.namesFlux()
+			.subscribe(name -> {
+				System.out.println("The name is: " + name);
+			});
+		
+	}
 	
 	public Flux<String> namesFlux() {
 		
 		return Flux.fromIterable(List.of("Alex", "Ben", "Chloe")); // DB or a remote service call
-		
-	}
-	
-	public static void main(String[] args) {
-		
 		
 	}
 
