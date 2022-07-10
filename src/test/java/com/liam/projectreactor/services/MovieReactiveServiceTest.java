d03decfe57cf48f12d72e82ee9ab00269169410c
Liam Desmond <liamjdesmond84@gmail.com>
2022-07-10 16:51:30 -0500

    Adding testcase for MovieReactiveService

--- src/test/java/com/liam/projectreactor/services/MovieReactiveServiceTest.java
+++ src/test/java/com/liam/projectreactor/services/MovieReactiveServiceTest.java
@@ -1,8 +1,13 @@
 package com.liam.projectreactor.services;
 
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+
+import com.liam.projectreactor.models.Movie;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 //import org.springframework.beans.factory.annotation.Autowired;
 
 class MovieReactiveServiceTest {
@@ -23,10 +28,35 @@ class MovieReactiveServiceTest {
 		
 		
 		//when
-		
+		Flux<Movie> result = movieReactiveService.getAllMovies();
 		
 		
 		//then
+		StepVerifier.create(result)
+		.assertNext(x -> {
+			assertEquals("Batman Begins", x.getMovie().getName());
+			assertEquals(2, x.getReviewList().size());
+			
+			// name of the movie
+			// reviewList
+		})
+		.assertNext(x -> {
+			assertEquals("The Dark Knight", x.getMovie().getName());
+			assertEquals(2, x.getReviewList().size());
+			
+			// name of the movie
+			// reviewList
+		})
+		.assertNext(x -> {
+			assertEquals("The Dark Knight Rises", x.getMovie().getName());
+			assertEquals(2, x.getReviewList().size());
+			
+			// name of the movie
+			// reviewList
+		})
+		.verifyComplete();
+
+		
 	}
 
 }
