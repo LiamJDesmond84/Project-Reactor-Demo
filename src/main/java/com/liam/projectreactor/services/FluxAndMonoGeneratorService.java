9c43b2011d55d4457d0e2dee4aeaab47869790b9
Liam Desmond <liamjdesmond84@gmail.com>
2022-07-17 11:57:26 -0500

    Tested class name logging

--- src/main/java/com/liam/projectreactor/services/FluxAndMonoGeneratorService.java
+++ src/main/java/com/liam/projectreactor/services/FluxAndMonoGeneratorService.java
@@ -58,7 +58,7 @@ public class FluxAndMonoGeneratorService {
 
 		fluxAndMonoGenServ.rangeErrors()
 		.subscribe(x -> {
-			System.out.println("Subscriber Test:" + x);
+			System.out.println("Subscriber Test: " + x);
 		});
 		
 	}
