c1da6a2835568798587bca51c3e588cc9820c1f3
Liam Desmond <liamjdesmond84@gmail.com>
2022-07-31 16:38:07 -0500

    Updating Mockito test file

--- src/test/java/com/liam/projectreactor/services/MovieReactiveServiceMockTest.java
+++ src/test/java/com/liam/projectreactor/services/MovieReactiveServiceMockTest.java
@@ -2,10 +2,23 @@ package com.liam.projectreactor.services;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class MovieReactiveServiceMockTest {
+	
+	
+	@Mock
+	private MovieInfoService movieInfoService;
+	
+	@Mock
+	private ReviewService reviewService;
+	
+	
+	@InjectMocks
+	MovieReactiveService movieReactiveService;
 	
 	
 	@Test
