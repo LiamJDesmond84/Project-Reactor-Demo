677f839096f1ab6f09017e1745fc522a4a0e0d8f
Liam Desmond <liamjdesmond84@gmail.com>
2022-07-31 16:35:45 -0500

    Adding getAllMovies testcase

--- src/test/java/com/liam/projectreactor/services/MovieReactiveServiceMockTest.java
+++ src/test/java/com/liam/projectreactor/services/MovieReactiveServiceMockTest.java
@@ -1,5 +1,22 @@
 package com.liam.projectreactor.services;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class MovieReactiveServiceMockTest {
+	
+	
+	@Test
+	void GetAllMovies() {
+		//given
+		
+		
+		//when
+		
+		
+		//then
+	}
 
 }
