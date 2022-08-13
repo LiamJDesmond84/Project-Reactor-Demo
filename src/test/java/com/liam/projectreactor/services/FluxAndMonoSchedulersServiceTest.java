05e8521e360144e4108c831a0412a8165f8e43ce
Liam Desmond <liamjdesmond84@gmail.com>
2022-08-13 15:03:23 -0500

    Adding publishOn testcase

---
+++ src/test/java/com/liam/projectreactor/services/FluxAndMonoSchedulersServiceTest.java
@@ -0,0 +1,14 @@
+package com.liam.projectreactor.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FluxAndMonoSchedulersServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
