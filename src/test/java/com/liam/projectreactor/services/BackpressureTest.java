e8ece71eae317968202598c2b117557b415de8e3
Liam Desmond <liamjdesmond84@gmail.com>
2022-08-18 19:02:29 -0500

    Adding testbackpressure method

--- src/test/java/com/liam/projectreactor/services/BackpressureTest.java
+++ src/test/java/com/liam/projectreactor/services/BackpressureTest.java
@@ -1,8 +1,27 @@
 package com.liam.projectreactor.services;
 
+import org.junit.jupiter.api.Test;
 
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+@Slf4j
 public class BackpressureTest {
 	
 	
+	
+	@Test
+	void testBackPressure() {
+		
+		Flux<Integer> numberRange = Flux.range(1, 100);
+		
+		
+		numberRange
+			.subscribe(num -> {
+				log.info("The number is: " + num);
+			});
+			
+		
+	}
 
 }
