7c6b460a262e97f0bedff871629f54b588364f6e
Liam Desmond <liamjdesmond84@gmail.com>
2022-08-20 11:58:08 -0500

    Adding testbackpressure_drop method

--- src/test/java/com/liam/projectreactor/services/BackpressureTest.java
+++ src/test/java/com/liam/projectreactor/services/BackpressureTest.java
@@ -17,7 +17,7 @@ public class BackpressureTest {
 	
 	
 	@Test
-	void testBackPressure() {
+	void testBackPressure() { // Just showing backpressure
 		
 		Flux<Integer> numberRange = Flux.range(1, 100)
 			.log();
@@ -70,6 +70,56 @@ public class BackpressureTest {
 	
 	@Test
 	void testBackPressure_1() throws InterruptedException {
+		
+		Flux<Integer> numberRange = Flux.range(1, 100)
+			.log();
+		
+		
+		CountDownLatch latch = new CountDownLatch(1);
+		
+		numberRange
+		.subscribe(new BaseSubscriber<Integer>() {
+			
+			@Override
+			protected void hookOnSubscribe(Subscription subscription) {
+				request(2); // Just requesting 2 elements
+
+			}
+			
+			@Override
+			protected void hookOnNext(Integer value) {
+				log.info("Hook onNext: {}", value);
+				if (value %2 == 0 || value < 50) {
+					request(2);
+				}
+				else {
+					cancel();
+				}
+			}
+			
+			@Override
+			protected void hookOnComplete() {		
+			}
+			
+			@Override
+			protected void hookOnError(Throwable throwable) {
+			}
+			
+			@Override
+			protected void hookOnCancel() {
+				log.info("Inside of cancel");
+				latch.countDown();
+			}
+			
+		});
+		
+		assertTrue(latch.await(5L, TimeUnit.SECONDS)); // Latch stays open for 5(5L) seconds
+		
+	}
+	
+	
+	@Test
+	void testBackPressure_drop() throws InterruptedException {
 		
 		Flux<Integer> numberRange = Flux.range(1, 100)
 			.log();
