f39ff1dde55e8270443a071c21e4f0985c5de2e6
Liam Desmond <liamjdesmond84@gmail.com>
2022-08-28 11:18:44 -0500

    Adding hotPublisherTest testcase

--- src/test/java/com/liam/projectreactor/services/ColdAndHotPublisherTest.java
+++ src/test/java/com/liam/projectreactor/services/ColdAndHotPublisherTest.java
@@ -16,38 +16,56 @@ public class ColdAndHotPublisherTest {
 	@Test
 	void coldPublisherTest() {
 		
-		Flux<Integer> fluxRange = Flux.range(1, 10)
-				.flatMap(x -> Mono.just(x)
-					.subscribeOn(Schedulers.parallel())
-
-				).log();
+//		Flux<Integer> fluxRange = Flux.range(1, 10)
+//				.flatMap(x -> Mono.just(x)
+//					.subscribeOn(Schedulers.parallel())
+//
+//				).log();
+//		
+//		Flux<Integer> fluxRange2 = Flux.range(1, 10)
+//				.flatMap(x -> Mono.just(x)
+//					.subscribeOn(Schedulers.parallel())
+//
+//				).log();
+//		
+//		List<Integer> intRange = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//		
+//		
+//		fluxRange.subscribe(x -> System.out.println("Subscriber 1: " + x));
+//		
+//		fluxRange2.subscribe(x -> System.out.println("Subscriber 2: " + x));
+//		
+//		Flux<Integer> fluxRange1 = Flux.fromIterable(intRange)
+//				.flatMap(x -> 
+//					Mono.just(x)
+//					.subscribeOn(Schedulers.parallel())
+//
+//				)
+//				.map(x -> {
+//					log.info("Subscriber 3: " + x);
+//					return x;
+//				});
+//		
+//		fluxRange1.subscribe(x -> System.out.println("Subscriber 4: " + x));
 		
-		Flux<Integer> fluxRange2 = Flux.range(1, 10)
-				.flatMap(x -> Mono.just(x)
-					.subscribeOn(Schedulers.parallel())
-
-				).log();
 		
-		List<Integer> intRange = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-		
+		Flux<Integer> fluxRange = Flux.range(1, 10);
 		
 		fluxRange.subscribe(x -> System.out.println("Subscriber 1: " + x));
 		
-		fluxRange2.subscribe(x -> System.out.println("Subscriber 2: " + x));
-		
-		Flux<Integer> fluxRange1 = Flux.fromIterable(intRange)
-				.flatMap(x -> 
-					Mono.just(x)
-					.subscribeOn(Schedulers.parallel())
-
-				)
-				.map(x -> {
-					log.info("Subscriber 3: " + x);
-					return x;
-				});
-		
-		fluxRange1.subscribe(x -> System.out.println("Subscriber 4: " + x));
+		fluxRange.subscribe(x -> System.out.println("Subscriber 2: " + x));
 				
+	}
+	
+	
+	@Test
+	void hotPublisherTest() {
+		
+		Flux<Integer> fluxRange = Flux.range(1, 10);
+		
+		fluxRange.subscribe(x -> System.out.println("Subscriber 1: " + x));
+		
+		fluxRange.subscribe(x -> System.out.println("Subscriber 2: " + x));
 	}
 
 }
