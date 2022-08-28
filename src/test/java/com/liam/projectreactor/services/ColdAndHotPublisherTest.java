d370d8bd999a0367e2e3444d116994fdb2a7c402
Liam Desmond <liamjdesmond84@gmail.com>
2022-08-28 10:43:06 -0500

    Adding coldPublisherTest method

--- src/test/java/com/liam/projectreactor/services/ColdAndHotPublisherTest.java
+++ src/test/java/com/liam/projectreactor/services/ColdAndHotPublisherTest.java
@@ -1,5 +1,19 @@
 package com.liam.projectreactor.services;
 
+import org.junit.jupiter.api.Test;
+
+import reactor.core.publisher.Flux;
+
 public class ColdAndHotPublisherTest {
+	
+	
+	@Test
+	void coldPublisherTest() {
+		
+		Flux<Integer> fluxRange = Flux.range(1, 10);
+		
+		
+		fluxRange.subscribe(x -> System.out.println(x));
+	}
 
 }
