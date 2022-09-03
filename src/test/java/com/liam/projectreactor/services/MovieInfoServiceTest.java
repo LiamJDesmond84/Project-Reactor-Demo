7d82e03d3be4821873d52795db7e759775b49116
Liam Desmond <liamjdesmond84@gmail.com>
2022-09-03 18:14:42 -0500

    Updating retrieveAllMovieInfo_RestClient testcase

--- src/test/java/com/liam/projectreactor/services/MovieInfoServiceTest.java
+++ src/test/java/com/liam/projectreactor/services/MovieInfoServiceTest.java
@@ -3,6 +3,11 @@ package com.liam.projectreactor.services;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.liam.projectreactor.models.MovieInfo;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
 public class MovieInfoServiceTest {
 	
 	
@@ -22,10 +27,12 @@ public class MovieInfoServiceTest {
 		
 		
 		//when
-		
+		Flux<MovieInfo> movieInfoFlux = movieInfoService.retrieveAllMovieInfo_RestClient();
 		
 		//then
-		
+		StepVerifier.create(movieInfoFlux)
+			.expectNextCount(7)
+			.verifyComplete();
 		
 	}
 
