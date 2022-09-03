8b64bc04a546a6706f35592d0bb1db4b30608be2
Liam Desmond <liamjdesmond84@gmail.com>
2022-09-03 18:11:49 -0500

    Created retrieveAllMovieInfo_RestClient method

--- src/test/java/com/liam/projectreactor/services/MovieInfoServiceTest.java
+++ src/test/java/com/liam/projectreactor/services/MovieInfoServiceTest.java
@@ -1,8 +1,16 @@
 package com.liam.projectreactor.services;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
 
 public class MovieInfoServiceTest {
+	
+	
+	MovieInfoService movieInfoService = new MovieInfoService();
+	
+	WebClient webClient = WebClient.builder() // creates instance of WebClient that automatically connects to this baseUrl
+							.baseUrl("http://localhost:8080/movies")
+							.build();
 	
 	
 	@Test
