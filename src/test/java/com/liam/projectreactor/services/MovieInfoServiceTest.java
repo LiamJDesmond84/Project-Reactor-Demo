c6656a26ff7c0568769bbe5ddcb516408751a99e
Liam Desmond <liamjdesmond84@gmail.com>
2022-09-03 18:09:39 -0500

    Created MovieInfoServiceTest class

---
+++ src/test/java/com/liam/projectreactor/services/MovieInfoServiceTest.java
@@ -0,0 +1,22 @@
+package com.liam.projectreactor.services;
+
+import org.junit.jupiter.api.Test;
+
+public class MovieInfoServiceTest {
+	
+	
+	@Test
+	void retrieveAllMovieInfo_RestClient() {
+		
+		//given
+		
+		
+		//when
+		
+		
+		//then
+		
+		
+	}
+
+}
